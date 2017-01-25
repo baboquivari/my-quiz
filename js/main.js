168802e32674642e1db3ada52ed98ca06f7b6d86
Tony Sian <to.sian.san@gmail.com>
2017-01-25 10:42:31 +0000

    Making progress with jQuery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
-  $("h1").click(function () {
-  	console.log("clicked");			
- });
+	$("h1").click(function () {
+  		console.log("clicked");	
+   	});   
+}); 
+// SETUP TO SEE TEST IF CONSOLE BEING LOGGED WHEN H1 IS CLICKED
 
 
 
+
+/*
+$(document).ready(function() {
+    $("h1").mouseenter(function() {
+        $("h1").fadeTo("fast", 0.25);
+    });
+    $("h1").mouseleave(function() {
+        $("h1").fadeTo("fast", 1);
+        });
 });
+// CREATE AN EFFECT ON H1 WHEN MOUSE ENTERS AND LEAVES
+*/
+
+
+
+
+
--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
-  $("h1").click(function () {
-  	console.log("clicked");			
- });
+ 	$("h1").click(function () {
+  		console.log("clicked");			
+ 	});
+});
 
-
+$(document).ready(function() {
+    $(".answers").mouseenter(function() {
+        $(".answers").fadeTo("fast", 0.25);
+    });
+    $(".answers").mouseleave(function() {
+        $(".answers").fadeTo("fast", 1);
+        });
+});
 
